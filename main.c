faad5c526564bdfd7b8428626a5ba0861c1688b4
MorpKnight <christoffelsihombing@gmail.com>
2022-12-05 21:41:45 +0700

    Adding more function

--- main.c
+++ main.c
@@ -10,63 +10,98 @@ typedef struct BENDA{
     int temperatur;
 } RUMAH;
 
-int main(){
-    int i, n, *count, fd;
-    char time_now[20], temp;
-    RUMAH *benda;
+void display_time(){
     time_t s;
     struct tm* current_time;
-
     s = time(NULL);
     current_time = localtime(&s);
     printf("%02d:%02d\n", current_time->tm_hour, current_time->tm_min);
+}
 
-    printf("Ada berapa benda yang dapat di-automisasi: ");
-    scanf("%d", &n);
-    benda = (RUMAH*)malloc(n*sizeof(RUMAH));
-    count = (int*)malloc(n*sizeof(int));
+void mainmenu(int *pilihan){
+    printf("Selamat datang di Home Automation\n");
+    printf("Program ini akan membantu anda dalam mengatur keadaan rumah anda, melalui automisasi yang dilakukan terhadap perangkat rumah anda\n");
+    printf("\n\nSilakan pilih menu dibawah\n");
+    printf("1. Tambahkan perangkat baru\n");
+    printf("2. Lihat daftar perangkat\n");
+    printf("3. Hapus perangkat\n");
+    printf("4. Bantuan\n");
+    printf("5. Keluar\n");
 
-    printf("Untuk waktu nyala dan mati, formatnya HH:MM\n");
-    for(i=0; i<n; i++){
+    printf(">");
+    scanf("%d", pilihan);
+}
+
+void add_device(RUMAH *device, int *n, int *count){
+    int i;
+    display_time();
+    printf("Ada berapa perangkat yang ingin anda tambahkan?\n");
+    printf(">");
+    scanf("%d", n);
+    device = (RUMAH*)malloc(*n * sizeof(RUMAH));
+    count = (int*)malloc(*n * sizeof(int));
+
+    printf("Format waktu adalah HH:MM\n");
+    for(i = 0; i < *n; i++){
+        printf("Masukkan nama perangkat ke-%d\n", i+1);
         printf("Nama benda: ");
-        scanf("%s", benda[i].nama);
+        scanf("%s", &(device+i)->nama);
         printf("Waktu nyala: ");
-        scanf("%s", benda[i].waktu_nyala);
+        scanf("%s", &(device+i)->waktu_nyala);
         printf("Waktu mati: ");
-        scanf("%s", benda[i].waktu_mati);
+        scanf("%s", &(device+i)->waktu_mati);
         printf("Temperatur: ");
-        scanf("%d", &benda[i].temperatur);
+        scanf("%d", &(device+i)->temperatur);
+        printf("\n");
     }
 
-    //initialize every count
-    for(i=0; i<n; i++){
+}
+
+void check_device(RUMAH *device, int *n, int *count){
+    int i;
+    time_t s;
+    char waktu[20];
+    struct tm* current_time;
+
+    for(i = 0; i < *n; i++){
         count[i] = 0;
     }
-
-    while(1){
-        s = time(NULL);
-        current_time = localtime(&s);
-        sprintf(time_now, "%02d:%02d", current_time->tm_hour, current_time->tm_min);
-        for(i=0; i<n; i++){
-            if(strcmp(time_now, benda[i].waktu_nyala) == 0){
-                if(count[i] == 0){
-                    printf("Nyala %s\n", benda[i].nama);
-                    count[i] = 1;
-                }
-            } else if(strcmp(time_now, benda[i].waktu_mati) == 0){
-                if(count[i] == 1){
-                    printf("Mati %s\n", benda[i].nama);
-                    count[i] = 0;
-                }
-            }
-        }
-        //use nonblocking i/o to scan character to temp
-        read(0, &temp, 1);
-        if(temp == 'q'){
-            break;
-        }
-        sleep(10);
+    //printf device
+    printf("Daftar perangkat yang ada\n");
+    for(i = 0; i < *n; i++){
+        printf("Nama benda: %s\n", device[i].nama);
+        printf("Waktu nyala: %s\n", device[i].waktu_nyala);
+        printf("Waktu mati: %s\n", device[i].waktu_mati);
+        printf("Temperatur: %d\n", device[i].temperatur);
+        printf("\n");
     }
+
+
+    // while(1){
+    //     s = time(NULL);
+    //     current_time = localtime(&s);
+    //     sprintf(waktu, "%02d:%02d", current_time->tm_hour, current_time->tm_min);
+    //     for(i = 0; i < *n; i++){
+    //         printf("Nama benda: %s\n", (device+i)->nama);
+    //     }
+    //     printf("Waktu sekarang: %s\n", waktu);
+    //     sleep(1);
+    // }
+}
+
+int main(){
+    int i, n;
+    int *count, *pilihan;
+    char time_now[20], temp;
+    struct tm* current_time;
+    RUMAH *benda;
+    time_t s;
+    mainmenu(pilihan);
+    add_device(benda, &n, count);
+    check_device(benda, &n, count);
+    
+    printf("Terima kasih telah menggunakan program ini\n");
+
     free(benda);
     free(count);
     return 0;
